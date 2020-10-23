e6ac01fe4b650e14b38830e382644bd32748f14b
Nick Spain <nicholas.spain@stileeducation.com>
2020-10-23 16:46:06 +1100

    Squashed 'samael/' content from commit 0824f54
    git-subtree-dir: samael
    git-subtree-split: 0824f54e548da8fa674b9d435609948cf31e5edf

---
+++ src/xmlsec/mod.rs
@@ -0,0 +1,28 @@
+//
+// Source of xmlsec adapted from a separate project: https://github.com/voipir/rust-xmlsec
+// MIT Licence (Voipir Group): https://github.com/voipir/rust-xmlsec/blob/master/LICENSE
+
+//!
+//! Bindings for XmlSec1
+//!
+//! Modules reflect the header names of the bound xmlsec1 library
+//!
+#![deny(missing_docs)]
+
+#[doc(hidden)]
+pub use libxml::tree::document::Document as XmlDocument;
+#[doc(hidden)]
+pub use libxml::tree::node::Node as XmlNode;
+
+mod error;
+mod keys;
+mod xmldsig;
+mod xmlsec;
+
+// exports
+pub use self::error::XmlSecError;
+pub use self::error::XmlSecResult;
+pub use self::keys::XmlSecKey;
+pub use self::keys::XmlSecKeyFormat;
+pub use self::xmldsig::XmlSecSignatureContext;
+pub use self::xmlsec::XmlSecContext;
