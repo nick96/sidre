40f62df0eb4100e0ac66f03b71b59380581fe942
Nick Spain <nicholas.spain@stileeducation.com>
2021-01-03 20:03:23 +1100

    Bump rand version to 0.8.0 and handle change in Alhpanumeric distribution
    The distribution now samples u8's not chars so doesn't directly convert to a string. Convert u8's to chars.

--- sidre/src/login.rs
+++ sidre/src/login.rs
@@ -39,6 +39,7 @@ pub struct LoginRequestParams {
 fn random_string(len: usize) -> String {
     rand::thread_rng()
         .sample_iter(&rand::distributions::Alphanumeric)
+        .map(char::from)
         .take(len)
         .collect()
 }
