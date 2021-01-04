9838fab87c4aeac5fed067883a9d2ba96a77bddf
Nick Spain <nicholas.spain@stileeducation.com>
2021-01-04 22:09:05 +1100

    Add command line args to specify host and port to run sidre on

--- sidre/src/main.rs
+++ sidre/src/main.rs
@@ -1,4 +1,27 @@
+use std::net::SocketAddr;
+
 use sidre::{app, store};
+
+const HELP: &str = "
+sidre
+
+USAGE:
+    sidre [options]
+
+FLAGS:
+    -h, --help              Print this message
+
+OPTIONS
+    -p, --port             Port to run sidre on (default: 8080)
+    -h, --host             Host to ruin sidre on (default: 0.0.0.0)
+";
+
+struct AppArgs {
+    /// Port on which the app should run.
+    port: i32,
+    /// Host to run the app on.
+    host: String,
+}
 
 // Determine whether or not we should use the persistent store.
 //
@@ -9,16 +32,37 @@ fn should_use_persistent_store() -> bool {
     !cfg!(feature = "data-in-memory")
 }
 
+fn parse_args() -> Result<AppArgs, pico_args::Error> {
+    let mut pargs = pico_args::Arguments::from_env();
+    if pargs.contains(["-h", "--help"]) {
+        eprintln!("{}", HELP);
+        std::process::exit(1);
+    }
+
+    let args = AppArgs {
+        port: pargs.opt_value_from_str(["-p", "--port"])?.unwrap_or(8080),
+        host: pargs
+            .opt_value_from_str(["-h", "--host"])?
+            .unwrap_or_else(|| "0.0.0.0".into()),
+    };
+
+    Ok(args)
+}
+
 #[tokio::main]
 async fn main() {
+    let args = parse_args().expect("Failed to parse args");
+
     let store = if should_use_persistent_store() {
         unimplemented!()
     } else {
         store::MemoryStore::new().expect("Failed to construct in-memory store")
     };
 
+    let addr: SocketAddr = format!("{}:{}", args.host, args.port)
+        .parse()
+        .expect("Invalid address");
+
     // Run the app on 0.0.0.0 so that it works in a container.
-    warp::serve(app(store).await)
-        .run(([0, 0, 0, 0], 8080))
-        .await;
+    warp::serve(app(store).await).run(addr).await;
 }
