468592c48744d07ffeb3683a3ae1c9f4e92d6856
Nick Spain <nicholas.spain@stileeducation.com>
2021-01-09 16:39:17 +1100

    samael: Call XmlSecKey::from_memory with rust-xmlsec signature that requires an optional password

--- samael/src/crypto.rs
+++ samael/src/crypto.rs
@@ -431,7 +431,7 @@ pub(crate) fn reduce_xml_to_signed(
             let mut verified = false;
             for openssl_key in certs {
                 let key_data = openssl_key.to_der()?;
-                let key = XmlSecKey::from_memory(&key_data, XmlSecKeyFormat::CertDer)?;
+                let key = XmlSecKey::from_memory(&key_data, XmlSecKeyFormat::CertDer, None)?;
                 sig_ctx.insert_key(key);
                 verified = sig_ctx.verify_node(&sig_node)?;
                 if verified {
