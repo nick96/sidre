51af42e5126f2ac32d21492c66c35b0c194b5cb9
Nick Spain <nicholas.spain@stileeducation.com>
2021-01-10 16:49:09 +1100

    Ease running test SP and make it more configurable

--- test-service-provider/entrypoint.py
+++ test-service-provider/entrypoint.py
@@ -6,14 +6,24 @@ import base64
 import subprocess
 
 b64_cert = os.environ["B64_IDP_X509"]
+sp_entity_id = os.environ["SP_ENTITY_ID"]
+idp_entity_id = os.environ["IDP_ENTITY_ID"]
 settings_path = "saml/settings.json"
 
 with open(settings_path) as fh:
     settings = json.load(fh)
 
 settings["idp"]["x509cert"] = b64_cert
+settings["idp"]["entityId"] = idp_entity_id
+
+settings["sp"]["entityId"] = sp_entity_id
+
+settings_json = json.dumps(settings, indent=4)
 
 with open(settings_path, "w") as fh:
-    json.dump(settings, fh)
+    fh.write(settings_json)
 
+os.environ["FLASK_ENV"] = os.environ.get("FLASK_ENV", "development")
+
+print(f"Settings: {settings_json}")
 subprocess.run(["flask", "run", "-h", "0.0.0.0"])
